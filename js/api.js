b38c66d39f51426f51d699afcae9e7ae5516cc4c
Nikita Bobers <58427328+nikitabobers@users.noreply.github.com>
2020-03-16 13:19:30 +0100

    api key file

--- js/api.js
+++ js/api.js
@@ -1,20 +1,21 @@
+const myKey = config.MY_KEY;
 class Api {
-  constructor(city, country) {
-    this.apiKey = "8c88092407e92eadc1dd313b1e805290";
-    this.city = city;
-    this.country = country;
-  }
+    constructor(city, country) {
+        this.apiKey = myKey;
+        this.city = city;
+        this.country = country;
+    }
 
-  async getApi() {
-    const response = await fetch(
-      `https:api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}&units=metric&appid=${this.apiKey}`
-    );
-    const responseData = await response.json();
-    return responseData;
-  }
+    async getApi() {
+        const response = await fetch(
+            `https:api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}&units=metric&appid=${this.apiKey}`
+        );
+        const responseData = await response.json();
+        return responseData;
+    }
 
-  changeLocation(city, country) {
-    this.city = city;
-    this.country = country;
-  }
+    changeLocation(city, country) {
+        this.city = city;
+        this.country = country;
+    }
 }
